371e4437e0135f128bd2c9b1d9c482c6e1b5d360
Atif-R <77061644+Atif-R@users.noreply.github.com>
2024-09-25 12:16:09 +0400

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Defined constants
+#define ESPRESSO_BEANS 8
+#define ESPRESSO_WATER 30
+#define CAPPUCCINO_MILK 70
+#define MOCHA_MILK 160
+#define MOCHA_CHOCOLATE 30
+
+#define MIN_BEANS 10
+#define MIN_WATER 50
+#define MIN_MILK 50
+#define MIN_CHOCOLATE 20
+
+#define ADMIN_PASSWORD 1234
